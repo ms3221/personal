3ad0cf5246d5fb5aa6b08148a85a35aae82a18bc
jun <hyeongjun3221@gmail.com>
2022-11-03 08:59:08 +0900

    update

--- zoom_study/src/server.js
+++ zoom_study/src/server.js
@@ -1,15 +1,97 @@
+import http from "http";
+import express from "express";
+import path, { parse } from "path";
+import { WebSocketServer } from "ws";
+import { Server } from "socket.io";
 
-import express from 'express';
-import path from 'path';
 const __dirname = path.resolve();
 const app = express();
-app.set('view engine', "pug")
-app.set("views", __dirname + "/src/views")
-app.use('/public', express.static(__dirname + "/src/public"))
-app.get('/', (req,res)=>{
-    res.render('home')
-})
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+app.use("/public", express.static(__dirname + "/src/public"));
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
 
-const handleListen = ()=>console.log(`Listening on http://localhost:3000`)
-app.listen(3000,handleListen)
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  // const sids = wsServer.sockets.adapter.sids;
+  // const rooms = wsServer.sockets.adapter.rooms;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+wsServer.on("connection", (socket) => {
+  socket["nickname"] = "Anon";
+  socket.onAny((event) => {
+    console.log(`socket event: ${event}`);
+  });
+
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+    wsServer.sockets.emit("room_change", publicRooms());
+  });
+
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
+    );
+  });
+
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
+  });
+
+  socket.on("new_message", (message, roomName, done) => {
+    socket.to(roomName).emit("new_message", `${socket.nickname}: ${message}`);
+    done();
+  });
+  socket.on("nickname", (nickname) => {
+    socket["nickname"] = nickname;
+  });
+});
+/**
+ * 11/01 webSocket을 이용해서 real-time chat기능을 구현
+ * 앞으로는 socke.io를 이용해서 구현할 예정
+ */
+// const wss = new WebSocketServer({ server: server });
+// const sockets = [];
+// wss.on("connection", (socket) => {
+//   sockets.push(socket);
+//   socket["nickname"] = "Anon";
+//   console.log("connected to Browser");
+//   socket.on("close", () => console.log("Disconnected from the browser"));
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg.toString("utf-8"));
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname}: ${message.payload}`)
+//         );
+//         break;
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//         break;
+//     }
+//   });
+// });
+
+httpServer.listen(3000, handleListen);
